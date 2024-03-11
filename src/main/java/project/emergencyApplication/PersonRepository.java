87e17ee8caa54cc21dfdc568792e2d1ec95fdcea
Lee ChanHui <ing3616062@naver.com>
2024-03-11 23:08:19 +0900

    Feat: First

---
+++ src/main/java/project/emergencyApplication/PersonRepository.java
@@ -0,0 +1,10 @@
+package project.emergencyApplication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+
+}
