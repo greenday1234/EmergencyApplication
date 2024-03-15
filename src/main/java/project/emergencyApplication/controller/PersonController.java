8023c0e607f92392a820b9f59e8f891f1f0b63a8
Lee ChanHui <ing3616062@naver.com>
2024-03-15 21:35:37 +0900

    feat: Swagger OpenAPI 사용

--- src/main/java/project/emergencyApplication/controller/PersonController.java
+++ src/main/java/project/emergencyApplication/controller/PersonController.java
@@ -1,7 +1,5 @@
 package project.emergencyApplication.controller;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +8,8 @@ import project.emergencyApplication.dto.Person;
 @RestController
 public class PersonController {
 
-    @ApiOperation(value="Find person by ID", notes="Provide an ID to look up a specific person from the people data.", response= Person.class)
     @GetMapping("/people/{id}")
-    public Person findPersonById(@ApiParam(value="ID value for the person you need to retrieve", required=true) @PathVariable Long id) {
+    public Person findPersonById(@PathVariable Long id) {
         // Method implementation
         return new Person();
     }
