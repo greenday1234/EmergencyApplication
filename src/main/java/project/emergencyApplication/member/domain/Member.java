6246ac021ba1df25cb6f46f427df92cc01b2abe5
Lee ChanHui <ing3616062@naver.com>
2024-03-17 21:33:44 +0900

    feat: repository

--- src/main/java/project/emergencyApplication/member/domain/Member.java
+++ src/main/java/project/emergencyApplication/member/domain/Member.java
@@ -1,6 +1,8 @@
 package project.emergencyApplication.member.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -8,7 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Member {
 
