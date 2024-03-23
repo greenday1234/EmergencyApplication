b535f7734fac195f69b8502590a316158eebaec8
Lee ChanHui <ing3616062@naver.com>
2024-03-23 18:23:42 +0900

    test

--- src/main/java/project/emergencyApplication/auth/jwt/AppleController.java
+++ src/main/java/project/emergencyApplication/auth/jwt/AppleController.java
@@ -3,20 +3,18 @@ package project.emergencyApplication.auth.jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping
 public class AppleController {
 
     private final AppleOauthService appleOauthService;
 
-    @PostMapping("43.203.54.29:8080/api/apple/user")
+    @PostMapping("api/apple/user")
     public ResponseEntity<Object> getAppleUser(@RequestBody HashMap<String, Object> appleToken) {
 
         AppleUser appleUser = appleOauthService.createAppleUser(String.valueOf(appleToken.get("id_token")));
