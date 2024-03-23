94623b8227c2fd4966f7bfd203d13e4855a5ac20
Lee ChanHui <ing3616062@naver.com>
2024-03-23 18:32:52 +0900

    test

--- src/main/java/project/emergencyApplication/auth/jwt/AppleController.java
+++ src/main/java/project/emergencyApplication/auth/jwt/AppleController.java
@@ -1,7 +1,11 @@
 package project.emergencyApplication.auth.jwt;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,9 +14,13 @@ public class AppleController {
 
     private final AppleOauthService appleOauthService;
 
-    @PostMapping("api/apple/user")
-    public String getAppleUser() {
+    @PostMapping("/api/apple/user")
+    public ResponseEntity<Object> getAppleUser(@RequestBody HashMap<String, Object> appleToken) {
 
-        return "ok";
+        AppleUser appleUser = appleOauthService.createAppleUser(String.valueOf(appleToken.get("id_token")));
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(appleUser);
     }
 }
