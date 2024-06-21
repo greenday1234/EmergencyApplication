fbd4042da7bd9efb0ffaf4d17766bbde59b7e1fe
Lee ChanHui <ing3616062@naver.com>
2024-06-22 00:46:58 +0900

    Test

--- src/main/java/project/emergencyApplication/auth/jwt/apple/AppleClient.java
+++ src/main/java/project/emergencyApplication/auth/jwt/apple/AppleClient.java
@@ -1,7 +1,7 @@
 package project.emergencyApplication.auth.jwt.apple;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * JWK 리스트 받아오기
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name = "apple-public-key", url = "https://appleid.apple.com")
 public interface AppleClient {
 
-    @GetMapping("/auth/keys")
+    @PostMapping("/auth/keys")
     ApplePublicKeys getApplePublicKeys();
 }
