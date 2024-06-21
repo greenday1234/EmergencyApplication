3f6fed4c6e9a863054bfed8ff2b752d3fe75a7b0
Lee ChanHui <ing3616062@naver.com>
2024-06-22 02:07:26 +0900

    test

--- src/main/java/project/emergencyApplication/auth/jwt/apple/AppleClient.java
+++ src/main/java/project/emergencyApplication/auth/jwt/apple/AppleClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * JWK 리스트 받아오기
  */
-@FeignClient(name = "apple-public-key", url = "appleid.apple.com")
+@FeignClient(name = "apple-public-key", url = "https://appleid.apple.com")
 public interface AppleClient {
 
     @GetMapping("/auth/keys")
